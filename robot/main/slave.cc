1b040d839b8b8d9c5efe5073f5dce49bd935ca5a
Lintang Sutawika <lintang.adyuta@ui.ac.id>
2015-05-08 02:09:17 +0700

    slave program should be working, will upload it later on

--- robot/main/slave.cc
+++ robot/main/slave.cc
@@ -1,9 +1,10 @@
 #include <arduino/Arduino.h>
-#include <arduino/Arduino.h>
 #include <comm/arduino_mavlink_packet_handler.hpp>
 #include <comm/custom.h>
 #include <actuator/sc.h>
 #include <sensor/two_phase_incremental_encoder.hpp>
+
+#define definedSpeed 100 //RPM
 
 int timer1_counter;
 const int pwm_pin = 11;
@@ -39,14 +40,13 @@ bool resetFlag=0;
 long error=0,thetaDot=0;
 float u_k;
 byte buffer[7] = {0,0,0,0,0,0,0};
+int theta[100];
+int theta_dot[100];
 
 //Array to place measured data
 byte dataFrom_Encoder[3] = {0xCE,0,0};
 int tempRead_Encoder = 0;
-int arrayIndex = 0;
-bool feedbackRequest = 0;
-bool sendRequest = 0;
-bool requestIs_Valid = 1;
+bool sendFlag=0;
 int i;
 
 
@@ -79,11 +79,15 @@ void setup()
 
 ISR(TIMER1_COMPA_vect)        // interrupt service routine 
 {
-  //interrupts();
   
-  // if(buffer[3] == 0xCC) sc.set_speed(-speed);//u_k = speed;//speed = (-1)*speed;
-    // else if(buffer[3] == 0x0C) sc.set_speed(speed);//u_k = -speed;
-  tempRead_Encoder = sc.set_speed(speed);
+  theta_dot[i] = sc.set_speed(speed);
+  theta[i] = sc.get_pos();
+  if(i < 100){
+    i++; 
+  }
+  else {
+    sendFlag =1; i = 101;
+  }
   
   // if(requestIs_Valid == 0){
     
@@ -118,31 +122,20 @@ int main() {
   int incoming_byte = 0;
   
   int flag=0;
-  // speed = 71;//
+  int j = 0;
   while (true) {
 
-    if (Serial.available() >= 7) {
-      if(Serial.read() == 0xCE ){
-      buffer[1] = Serial.read(); //LSB
-      buffer[2] = Serial.read(); //MSB
-      buffer[3] = Serial.read(); //Feedback request flag
-      buffer[4] = Serial.read(); //Checksum
-      buffer[5] = Serial.read(); //Checksum
-      buffer[6] = Serial.read();
-        }
+    if(sendFlag == 0) speed = definedSpeed;
+    else {
+      speed = 0;
+      sendFlag=0;
+      for(j = 0; j<100; j++){
+        Serial.print(theta[j]);
+        Serial.print(",");
+        Serial.println(theta_dot[j]);
       }
-
-    
-    bufferSpeed = ((buffer[2]<<8) | (buffer[1]));//bufferSpeed;
-    speed = bufferSpeed;
-    //Reset the encoder every time the setpoint is 0 RPM
-    // if(speed == 0) {if(resetFlag == 0) {sc.reset(); resetFlag =1;}} else {resetFlag = 0;} //if speed is not 0, the resetFlag is set to 0 again
-    // dataFrom_Encoder[1] = (tempRead_Encoder & 0x00FF);
-    // dataFrom_Encoder[2] = ((tempRead_Encoder & 0xFF00)>>8);
-    // Serial.write(dataFrom_Encoder,3);
-    Serial.println(tempRead_Encoder);
-    // sc.testing_encoder();
-    
+    break; //Exit loop;
+    }    
 
   }
 
